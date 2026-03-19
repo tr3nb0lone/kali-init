3cec0291ea0b0c2222fa790f22a34ac575b7807d
tr3nb0lone <tr3nacetate@proton.me>
2026-03-19 23:10:46 +0300

    This ain't it, chief.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-
 # forces overwriting of existing configs:
 alias cp='cp -f'
 
-echo -e "\t Script initialized!\n"
+echo -e "\n[INFO] Script initialized!\n"
 home=${HOME}
 
 # INFO: essentials!
@@ -14,85 +13,31 @@ sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-w
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 
-# prepare the destination directories:
-echo -e "\t Now creating the destination directories:\n"
-config_dirs=(
-    "i3"
-    "feh"
-    "utils"
-    "i3-autodisplay"
-    "picom"
-    "kitty"
-    "tmux"
-    "polybar"
-    "rofi"
-    "dunst"
-    "wallpapers"
-    "yazi"
-)
-
-for dir in "${config_dirs[@]}"; do
-    mkdir -p "$home/.config/$dir/" || echo -e "$dir already exists. boohoo.\n"
-done
-
-# list of dotfiles and destinations
-echo -e "\t Copying the dotfiles onto their respective destination:\n"
-declare -A dotfiles=(
-    ["./dotfiles/dunst/"]=".config/dunst/"
-    ["./dotfiles/feh/"]=".config/feh/"
-    ["./dotfiles/ghostty/"]=".config/ghostty/"
-    ["./dotfiles/i3/"]=".config/i3/"
-    ["./dotfiles/i3-autodisplay/"]=".config/i3-autodisplay/"
-    ["./dotfiles/kitty/"]=".config/kitty/"
-    ["./dotfiles/polybar/"]=".config/polybar/"
-    ["./dotfiles/picom/"]=".config/picom/"
-    ["./dotfiles/rofi/"]=".config/rofi/"
-    ["./dotfiles/tmux/"]=".config/tmux/"
-    ["./dotfiles/utils/"]=".config/utils/"
-    ["./dotfiles/wallpapers/"]=".config/wallpapers/"
-    ["./dotfiles/yazi/"]=".config/yazi/"
-    ["./dotfiles/aliases"]=".zsh_aliases"
-)
-
-# copy each dotfile to its destination
-for src in "${!dotfiles[@]}"; do
-    dest="$home/${dotfiles[$src]}"
-    cp -rf "$src" "$dest"
-done
+# drop dotfiles the lazy way!
+cp ./dotfiles/* "$home/.config"
+cp ./aliases ~/.zsh_aliases
 
 cd "$home" || exit
 
-echo -e "\t Now cloning TPM-redux:\n"
-git clone https://github.com/RyanMacG/tpm-redux.git ~/.tmux/plugins/tpm-redux
-
-# Volume Control
-cd "$home" || exit
-git clone https://github.com/hastinbe/i3-volume.git ~/i3-volume
-
-# nvim
-echo -e "\t Onto Installing Nvim:\n"
-# clean previous config/cache:
+echo -e "[INFO] Onto Installing nvim:\n"
+# clean previous config/cache, then install
 rm -rf ~/.config/nvim
 rm -rf ~/.cache/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
-
-# replace it with my own
 git clone https://github.com/tr3nb0lone/nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
-# fonts:
+# favorite fonts:
 mkdir -p ~/.local/share/fonts/
-
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
-
 unzip Iosevka.zip -d ~/.local/share/fonts/
-unzip RobotoMono.zip -d ~/.local/share/fonts/
+fc-cache -fv # whoa!
 
-fc-cache -fv
+cd "$home" || exit
 
-# install ohMyZsh:
+# terminal essentials:
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/RyanMacG/tpm-redux.git ~/.tmux/plugins/tpm-redux
 
-echo -e "setup is ready!"
+echo -e "[INFO] setup is ready!"
 sudo reboot
--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -6,12 +6,5 @@ killall -q polybar
 # wait till the processes have been shut:
 while pgrep -u  $UID -x polybar >/dev/null; do sleep 1;done
 
-# check and launch the external monitor's Bar, monibar:
-if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
-	# Launch a bar named mainbar and monibar, sorry if anyone's confused :(
-	polybar monibar &
-	polybar mainbar &
-else
-	# launch laptop's bar only!
-	polybar mainbar &
-fi
+# launch the bar:
+polybar mainbar &
