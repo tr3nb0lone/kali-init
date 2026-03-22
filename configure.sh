45b90bd8421550841ecdf6db5641fab4be5c8373
tr3nb0lone <tr3nacetate@proton.me>
2026-03-22 22:54:32 +0300

    feat: nix

--- configure.sh
+++ configure.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
-
-# forces overwriting of existing configs:
-alias cp='cp -f'
+set -Eeuo pipefail
 
 echo -e "\n[INFO] Script initialized!\n"
 home=${HOME}
 
 # INFO: essentials!
 sudo apt update
-sudo apt-get install -y wget curl git thunar
-sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt-get install -y arandr flameshot polybar arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 
 # drop dotfiles the lazy way!
-cp ./dotfiles/* "$home/.config"
-cp ./aliases ~/.zsh_aliases
+cp -rf ./dotfiles/* "$home/.config"
+cp -rf ./aliases ~/.zsh_aliases
 
 cd "$home" || exit
 
@@ -37,7 +34,17 @@ cd "$home" || exit
 
 # terminal essentials:
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/RyanMacG/tpm-redux.git ~/.tmux/plugins/tpm-redux
+git clone https://github.com/RyanMacG/tpm-redux.git ~/.tmux/plugins/tpm-redux || echo -e "tpm-redux might alredy be installed!"
+
+# install Nix:
+echo -e "[INFO] Installing Nix:\n "
+VERSION='2.31.3' # replace it with the latest version
+URL="https://releases.nixos.org/nix/nix-${VERSION}/install"
+MY_CONF="./dotfiles/nix/nix.conf"
+sh <(curl --location "${URL}") \
+     --daemon \
+     --no-channel-add \
+     --nix-extra-conf-file ${MY_CONF}
 
 echo -e "[INFO] setup is ready!"
 sudo reboot
